2121c1f606c8a836ad1b98beccb880989302deb2
Jorge Kimura <kimurajorge2001@gmail.com>
2022-05-13 13:13:17 -0300

    Arquivo JS

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// PARTE 1 - ROLAGEM DOS DADOS
+
 function numerosAleatorios (min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -16,35 +18,39 @@ const resultado = {
     11: 0,
     12: 0 
 }
-let soma = 0
+let soma = 0 // variavel auxiliar para realizar a soma entre os valores
 
-for(let i = 1; i <= 1000; i++){ // 1 a 1000
-    const dado1 = numerosAleatorios(1,6) // dados 
-    const dado2 = numerosAleatorios(1,6) 
+for(let i = 1; i <= 1000; i++){ // loop para percorrer entre 1 a 1000 rolagens
+    const dado1 = numerosAleatorios(1,6) // 1 dado que será jogado
+    const dado2 = numerosAleatorios(1,6) // 2 dado que será jogado
 
-    soma = dado1 + dado2 // 2 e 12 
+    soma = dado1 + dado2 // soma entre o primeiro e segundo
                             // valor                
     resultado[`${soma}`] = (resultado[`${soma}`] + 1) // [] é para valores que não aceitos na sintaxe do objeto ~ templates string 
     
 }
+
+for(let chave in resultado){ // loop para percorrer o objeto 
+    const pResultados = document.createElement('p') // criou-se um paragrafo 
+    pResultados.innerText = `${chave}: ${resultado[chave]}` // para colocar o objeto em um elemento HTNL 
+    document.body.appendChild(pResultados) // acrescentou-se ao site utilizando appendChild
+}
+
 console.log(resultado)
 
-// PARTE 2 - PERGUNTAR PARA COACH 
+// PARTE 2 - MOSTRAR NO SITE EM FORMA DE GRÁFICO
 
-const minhaBarra = document.createElement('div')
+let larguraDaBarra = 0 // variavel auxiliar
 
-minhaBarra.classList.add('barra')
+for(let chave in resultado){ // loop vai criar uma div e deve-se setar o atributo css
+    const minhaBarra = document.createElement('div') // criando uma div para cada elemento dentro do objeto 
 
-let larguraDaBarra = 0 
-for(let chave in resultado){
-    //console.log(resultado)
-    if(resultado[chave] == resultado[2]){ // como deixar de forma mais dinamica que passe por todos as chaves
-    larguraDaBarra += resultado[chave]
+    minhaBarra.classList.add('barra') // atribuindo uma classe para estilizar no css
+    
+    larguraDaBarra = resultado[chave] // salvando largura na variavel 
+    
+    minhaBarra.style.width = `${larguraDaBarra}px` // estilizando utilizando o template string 
+    // para inserir variaveis no elemento html é através de template string
+
+    document.body.appendChild(minhaBarra) // adicionando ao site
 }
-}
-
-minhaBarra.style.width = `${larguraDaBarra}px`
-
-document.body.appendChild(minhaBarra)
-
-console.log(minhaBarra)
